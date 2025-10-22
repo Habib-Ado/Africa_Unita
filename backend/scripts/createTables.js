99ff65b0f9253f0b236525b3ffb3d08ed68b7173
HabibAdo <adobinesse@gmail.com>
2025-10-22 17:00:30 +0100

    Update createTables.js

--- backend/scripts/createTables.js
+++ backend/scripts/createTables.js
@@ -38,7 +38,6 @@ async function createTables() {
 
         // Lire le schéma
         const schemaPath = path.join(__dirname, '../database/schema.sql');
-        
         const schema = fs.readFileSync(schemaPath, 'utf8');
 
         console.log('📄 Exécution du schéma SQL...');
